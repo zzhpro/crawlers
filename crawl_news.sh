b97a7409b8c39c6c1984c9b69cbfeadbeab42fbd
zzhpro <56196563+zzhpro@users.noreply.github.com>
2022-04-02 14:17:49 +0800

    Update crawl_news.sh

--- crawl_news.sh
+++ crawl_news.sh
@@ -1,5 +1,5 @@
+echo `date` START >> /home/data/zhangzhenhao-21/crawlers/log/news.log
 source /home/zhangzhenhao-21/miniconda3/etc/profile.d/conda.sh
 conda activate crawl
-cd /home/data/zhangzhenhao-21/crawlers
-python src/mitcsail.py
-echo `date` Done >> log/news.log
+python /home/data/zhangzhenhao-21/crawlers/src/mitcsail.py
+echo `date` Done >> /home/data/zhangzhenhao-21/crawlers/log/news.log
